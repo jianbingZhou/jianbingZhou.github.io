cdc562a9a90297fadc49cc427740d5d1a9a2874a
zhoujianbing <zhoujianbing@qq.com>
2022-08-04 15:01:08 +0800

    '上传代码'

---
+++ src/resources/textures.js
@@ -0,0 +1,62 @@
+let billboardTextures = {};
+// 第一块展板图片地址
+billboardTextures.blogTexture = "../src/jsm/blog.png";
+// 第二块展板图片地址
+billboardTextures.musicTexture = "../src/jsm/music.png";
+// 第三块展板图片地址
+billboardTextures.fundTexture = "../src/jsm/fund.png";
+
+let boxTexture = {};
+// 链接的图标地址
+boxTexture.Github = "../src/jsm/githubLogo.png";
+boxTexture.BiliBili = "../src/jsm/BiliBili.png";
+boxTexture.QQ = "../src/jsm/qq.png";
+boxTexture.mail = "../src/jsm/envelope.png";
+boxTexture.reactIcon = "../src/jsm/react.png";
+boxTexture.allSkills = "../src/jsm/allSkills.png";
+boxTexture.lensFlareMain = "../src/jsm/lensflare0.png";
+boxTexture.skrillex = "../src/jsm/skrillex.png";
+boxTexture.edmText = "../src/jsm/EDM.png";
+
+// 砖块材质
+let stoneTexture = "../src/jsm/stone.png";
+// 展板木头材质
+let woodTexture = "../src/jsm/woodTexture.jpg";
+
+// 文字图片地址
+let inputText = {};
+inputText.terpSolutionsText = "../src/jsm/terp-solutions-text.png";
+inputText.activities = "../src/jsm/activities_text.png";
+inputText.bagholderBetsText = "../src/jsm/bagholderbets-text.png";
+inputText.homeSweetHomeText = "../src/jsm/home-sweet-home-text.png";
+inputText.staticPortfolio = "../src/jsm/static-portfolio.png";
+inputText.pcControl = "../src/jsm/pc-control.png";
+inputText.mobileControl = "../src/jsm/mobile-control.png";
+inputText.link = "../src/jsm/link.png";
+
+//SVG
+let SVG = {};
+SVG.reactLogo = "../src/jsm/react-svg.svg";
+
+// 链接的跳转地址
+let URL = {};
+URL.blog = "https://www.jianshu.com/u/32c7d4bfec2a";
+URL.ryanfloyd = "https://huabyte.com";
+URL.fund = "https://github.com/AirHua-byte/FundSpider";
+URL.gitHub = "https://github.com/jianbingZhou";
+URL.BiliBili = "https://space.bilibili.com/334173842/";
+URL.email =
+  "http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=rp2XnJqelpidm_7f34DNwcM";
+URL.music = "https://music.huabyte.com";
+URL.devTo =
+  "https://qm.qq.com/cgi-bin/qm/qr?k=OksZ-VmLQBuzcYh4iqUarrmFcQorA3SM&noverify=0";
+
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
